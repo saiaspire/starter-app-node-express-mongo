d7fd1516cd9b223b2f0711485981c5ceac3923e6
Saikrishnan Ranganathan <saiaspire@gmail.com>
2014-11-22 16:17:05 +0800

    Refactors tests, make tests independent, use promises to resolve all errors

--- test/authTests.js
+++ test/authTests.js
@@ -1,14 +1,16 @@
 var auth = require('../lib/auth');
 var should = require('should');
 var request = require('request');
-var jar = request.jar();
 var mongoose = require('mongoose');
+var q = require('q');
 
 var PORT = 3000;
 var MONGODB_CONNECTION_STRING = 'mongodb://localhost/expensior_test';
 var BASE_URL = "http://localhost:" + PORT;
 
 var server;
+
+var User = require('../models/user')
 
 describe('Authentication Module Tests', function () {
 
@@ -31,8 +33,13 @@ describe('Authentication Module Tests', function () {
         });
     });
 
+    beforeEach(function (done) {
+        User.remove(done)
+    });
+
     it('should be ready', function () {
         should.exist(auth);
+        should.exist(mongoose.connection)
     });
 
     it('should throw an error when creating a user without username', function (done) {
@@ -191,9 +198,9 @@ describe('Authentication Module Tests', function () {
     });
 
     it('should create an user and session when everything is in order', function (done) {
-        var registrationJar = request.jar();
+        var cookies = request.jar();
         request.post(BASE_URL + '/user', {
-            json: true, jar: registrationJar, body: {
+            json: true, jar: cookies, body: {
                 username: "test",
                 password: "testingpassword",
                 name: "Tester",
@@ -206,69 +213,94 @@ describe('Authentication Module Tests', function () {
             response.statusCode.should.equal(201);
             body.should.have.property("status");
             body.status.should.equal("Successfully registered user");
-            registrationJar.getCookies(BASE_URL).pop().key.should.equal("expensior_session");
+            cookies.getCookies(BASE_URL).pop().key.should.equal("expensior_session");
             done();
         });
     });
 
     it('should not create an username if it already exists', function (done) {
-        request.post(BASE_URL + '/user', {
-            json: true, body: {
-                username: "test",
-                password: "testingpassword",
-                name: "Tester",
-                email: "test2@test.com"
-            }
-        }, function (err, response, body) {
-            if (err) {
-                done(err);
-            }
-            response.statusCode.should.equal(409);
-            body.should.have.property("error");
-            body.error.should.have.property("message");
-            body.error.message.should.equal("Username already exists");
-            done();
-        });
+        var username = "test",
+            password = "testingpassword",
+            name = "Tester",
+            email = "test@test.com";
+
+        createTestUser(username, password, name, email).then(function () {
+            var deferred = q.defer();
+            request.post(BASE_URL + '/user', {
+                json: true, body: {
+                    username: username,
+                    password: password,
+                    name: name,
+                    email: "test2@test.com"
+                }
+            }, function (err, response, body) {
+                if (err) {
+                    deferred.reject(err);
+                }
+                response.statusCode.should.equal(409);
+                body.should.have.property("error");
+                body.error.should.have.property("message");
+                body.error.message.should.equal("Username already exists");
+                deferred.resolve();
+            });
+            return deferred.promise;
+        }).then(done, done);
     });
 
     it('should not create an user if the email is already registered to someone else', function (done) {
-        request.post(BASE_URL + '/user', {
-            json: true, body: {
-                username: "testemail",
-                password: "testingpassword",
-                name: "Tester",
-                email: "test@test.com"
-            }
-        }, function (err, response, body) {
-            if (err) {
-                done(err);
-            }
-            response.statusCode.should.equal(409);
-            body.should.have.property("error");
-            body.error.should.have.property("message");
-            body.error.message.should.equal("Email already registered to another account");
-            done();
-        });
+        var username = "test",
+            password = "testingpassword",
+            name = "Tester",
+            email = "test@test.com";
+
+        createTestUser(username, password, name, email).then(function () {
+            var deferred = q.defer();
+            request.post(BASE_URL + '/user', {
+                json: true, body: {
+                    username: "test2",
+                    password: "testingpassword",
+                    name: "Tester",
+                    email: email
+                }
+            }, function (err, response, body) {
+                if (err) {
+                    deferred.reject(err);
+                }
+                response.statusCode.should.equal(409);
+                body.should.have.property("error");
+                body.error.should.have.property("message");
+                body.error.message.should.equal("Email already registered to another account");
+                deferred.resolve();
+            });
+            return deferred.promise;
+        }).then(done, done);
     });
 
     it('should not login if password is incorrect', function (done) {
-        request.post(BASE_URL + '/session', {
-            json: true, body: {
-                username: "test",
-                password: "wrongpassword",
-                name: "Tester",
-                email: "test@test.com"
-            }
-        }, function (err, response, body) {
-            if (err) {
-                done(err);
-            }
-            response.statusCode.should.equal(401);
-            body.should.have.property("error");
-            body.error.should.have.property("message");
-            body.error.message.should.equal("Username or password is invalid");
-            done();
-        });
+        var username = "test",
+            password = "testingpassword",
+            name = "Tester",
+            email = "test@test.com";
+
+        createTestUser(username, password, name, email).then(function () {
+            var deferred = q.defer();
+            request.post(BASE_URL + '/session', {
+                json: true, body: {
+                    username: username,
+                    password: "wrongpassword"
+                }
+            }, function (err, response, body) {
+                if (err) {
+                    deferred.reject(err);
+                }
+                response.statusCode.should.equal(401);
+                body.should.have.property("error");
+                body.error.should.have.property("message");
+                body.error.message.should.equal("Username or password is invalid");
+                deferred.resolve()
+            });
+            return deferred.promise;
+        }).then(done, done);
     });
 
     it('should not login if user is not registered', function (done) {
@@ -292,61 +324,90 @@ describe('Authentication Module Tests', function () {
     });
 
     it('should login and set session cookie if username and password is correct', function (done) {
-        request.post(BASE_URL + '/session', {
-            json: true, jar: jar, body: {
-                username: "test",
-                password: "testingpassword",
-                name: "Tester",
-                email: "test@test.com"
-            }
-        }, function (err, response, body) {
-            if (err) {
-                done(err);
-            }
-            response.statusCode.should.equal(201);
-            body.should.have.property("status");
-            body.status.should.equal("Logged in successfully");
-            jar.getCookies(BASE_URL).pop().key.should.equal("expensior_session");
-            done();
-        });
+        var username = "test",
+            password = "testingpassword",
+            name = "Tester",
+            email = "test@test.com";
+
+        var cookies = request.jar();
+
+        createTestUser(username, password, name, email).then(function () {
+            var deferred = q.defer();
+            request.post(BASE_URL + '/session', {
+                json: true, jar: cookies, body: {
+                    username: username,
+                    password: password
+                }
+            }, function (err, response, body) {
+                if (err) {
+                    deferred.reject(err)
+                }
+                response.statusCode.should.equal(201);
+                body.should.have.property("status");
+                body.status.should.equal("Logged in successfully");
+                cookies.getCookies(BASE_URL).pop().key.should.equal("expensior_session");
+                deferred.resolve();
+            });
+            return deferred.promise;
+        }).then(done, done);
     });
 
     it('should not login again if session exists', function (done) {
-        request.post(BASE_URL + '/session', {
-            json: true, jar: jar, body: {
-                username: "test",
-                password: "testingpassword",
-                name: "Tester",
-                email: "test@test.com"
-            }
-        }, function (err, response, body) {
-            if (err) {
-                done(err);
-            }
-            response.statusCode.should.equal(200);
-            body.should.have.property("status");
-            body.status.should.equal("Already logged in");
-            done();
-        });
+        var username = "test",
+            password = "testingpassword",
+            name = "Tester",
+            email = "test@test.com";
+
+        var cookies = request.jar();
+
+        createTestUser(username, password, name, email, cookies).then(function () {
+            var deferred = q.defer();
+            request.post(BASE_URL + '/session', {
+                json: true, jar: cookies, body: {
+                    username: username,
+                    password: password
+                }
+            }, function (err, response, body) {
+                if (err) {
+                    deferred.reject(err);
+                }
+                response.statusCode.should.equal(200);
+                body.should.have.property("status");
+                body.status.should.equal("Already logged in");
+                deferred.resolve();
+            });
+            return deferred.promise;
+        }).then(done, done);
     });
 
     it('should logout if session exists', function (done) {
-        request.del(BASE_URL + '/session', {
-            json: true, jar: jar, body: {}
-        }, function (err, response, body) {
-            if (err) {
-                done(err);
-            }
-            response.statusCode.should.equal(200);
-            body.should.have.property("status");
-            body.status.should.equal("Logged out successfully");
-            done();
-        });
+        var username = "test",
+            password = "testingpassword",
+            name = "Tester",
+            email = "test@test.com";
+
+        var cookies = request.jar();
+
+        createTestUser(username, password, name, email, cookies).then(function () {
+            var deferred = q.defer();
+            request.del(BASE_URL + '/session', {
+                json: true, jar: cookies, body: {}
+            }, function (err, response, body) {
+                if (err) {
+                    deferred.reject(err);
+                }
+                response.statusCode.should.equal(200);
+                body.should.have.property("status");
+                body.status.should.equal("Logged out successfully");
+                deferred.resolve();
+            });
+            return deferred.promise;
+        }).then(done, done);
     });
 
     it('should identify we are already logged out if session does not exist', function (done) {
         request.del(BASE_URL + '/session', {
-            json: true, jar: jar, body: {}
+            json: true, body: {}
         }, function (err, response, body) {
             if (err) {
                 done(err);
@@ -359,4 +420,22 @@ describe('Authentication Module Tests', function () {
     });
 });
 
+function createTestUser(username, password, name, email, cookies) {
+    var deferred = q.defer();
+    request.post(BASE_URL + '/user', {
+        json: true, jar: cookies, body: {
+            username: username,
+            password: password,
+            name: name,
+            email: email
+        }
+    }, function (err, response, body) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve({response: response, body: body});
+        }
+    });
+    return deferred.promise;
+}
 
